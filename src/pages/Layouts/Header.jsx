b2724b3d44758d63b9ba0a06176f03fe63c4461b
Nassim El M'harmache <n.elmharmache@gmail.com>
2025-09-28 14:26:36 +0100

    second commit

--- src/pages/Layouts/Header.jsx
+++ src/pages/Layouts/Header.jsx
@@ -13,44 +13,84 @@ const Header = () => {
           backgroundPosition: "center",
         }}
       >
-        <div className=" Mycontainer  text-dark flex justify-between items-center gap-[20px]" >
+        <div className=" Mycontainer  text-dark flex justify-between items-center gap-[20px]">
           <div className="flex justify-center items-start flex-col gap-[40px]">
             <h1 className="text-[50px] font-pB py-[20px]">Die Präsentation</h1>
             <div className="flex justify-center items-center flex-col gap-[20px]">
               <Link
-                className="bg-white  text-center font-pB p-[15px] w-[100%] border-[2px] border-black"
-                to={"/page1"}
+                to="/page1"
+                className="relative bg-white text-center font-pB p-[15px] w-[100%] border-[2px] border-black overflow-hidden group"
               >
-                Buckstabiren und Zahlen
+                <span className="relative z-10 transition-colors duration-500 group-hover:text-white text-[20px]">
+                  Buckstabiren und Zahlen
+                </span>
+                <span className="absolute top-0 left-0 w-full h-1/3 bg-black scale-x-0 group-hover:scale-x-100 origin-left transition-transform duration-500" />
+                <span className="absolute top-1/3 left-0 w-full h-1/3 bg-red scale-x-0 group-hover:scale-x-100 origin-left transition-transform duration-700" />
+                <span className="absolute top-2/3 left-0 w-full h-1/3 bg-yellow scale-x-0 group-hover:scale-x-100 origin-left transition-transform duration-1000" />
               </Link>
               <Link
-                className="bg-white  text-center font-pB p-[15px] w-[100%] border-[2px] border-black"
-                to={"/page2"}
+                to="/page1"
+                className="relative bg-white text-center font-pB p-[15px] w-[100%] border-[2px] border-black overflow-hidden group"
               >
-                Sich Verstellen
+                <span className="relative z-10 transition-colors duration-500 group-hover:text-white text-[20px]">
+                  Sich Verstellen
+                </span>
+                <span className="absolute top-0 left-0 w-full h-1/3 bg-black scale-x-0 group-hover:scale-x-100 origin-left transition-transform duration-500" />
+                <span className="absolute top-1/3 left-0 w-full h-1/3 bg-red scale-x-0 group-hover:scale-x-100 origin-left transition-transform duration-700" />
+                <span className="absolute top-2/3 left-0 w-full h-1/3 bg-yellow scale-x-0 group-hover:scale-x-100 origin-left transition-transform duration-1000" />
+              </Link>
+                   <Link
+                to="/page1"
+                className="relative bg-white text-center font-pB p-[15px] w-[100%] border-[2px] border-black overflow-hidden group"
+              >
+                <span className="relative z-10 transition-colors duration-500 group-hover:text-white text-[20px]">
+                  Nominativ und Akkusativ
+                </span>
+                <span className="absolute top-0 left-0 w-full h-1/3 bg-black scale-x-0 group-hover:scale-x-100 origin-left transition-transform duration-500" />
+                <span className="absolute top-1/3 left-0 w-full h-1/3 bg-red scale-x-0 group-hover:scale-x-100 origin-left transition-transform duration-700" />
+                <span className="absolute top-2/3 left-0 w-full h-1/3 bg-yellow scale-x-0 group-hover:scale-x-100 origin-left transition-transform duration-1000" />
               </Link>
               <Link
-                className="bg-white  text-center font-pB p-[15px] w-[100%] border-[2px] border-black"
-                to={"/page3"}
+                to="/page1"
+                className="relative bg-white text-center font-pB p-[15px] w-[100%] border-[2px] border-black overflow-hidden group"
               >
-                Die Monate und Die wochen und Die Uhr
+                <span className="relative z-10 transition-colors duration-500 group-hover:text-white text-[20px]">
+                  Die Monate und Die wochen und Die Uhr
+                </span>
+                <span className="absolute top-0 left-0 w-full h-1/3 bg-black scale-x-0 group-hover:scale-x-100 origin-left transition-transform duration-500" />
+                <span className="absolute top-1/3 left-0 w-full h-1/3 bg-red scale-x-0 group-hover:scale-x-100 origin-left transition-transform duration-700" />
+                <span className="absolute top-2/3 left-0 w-full h-1/3 bg-yellow scale-x-0 group-hover:scale-x-100 origin-left transition-transform duration-1000" />
+              </Link>
+
+              <Link
+                to="/page1"
+                className="relative bg-white text-center font-pB p-[15px] w-[100%] border-[2px] border-black overflow-hidden group"
+              >
+                <span className="relative z-10 transition-colors duration-500 group-hover:text-white text-[20px]">
+                  Stammbaum
+                </span>
+                <span className="absolute top-0 left-0 w-full h-1/3 bg-black scale-x-0 group-hover:scale-x-100 origin-left transition-transform duration-500" />
+                <span className="absolute top-1/3 left-0 w-full h-1/3 bg-red scale-x-0 group-hover:scale-x-100 origin-left transition-transform duration-700" />
+                <span className="absolute top-2/3 left-0 w-full h-1/3 bg-yellow scale-x-0 group-hover:scale-x-100 origin-left transition-transform duration-1000" />
               </Link>
               <Link
-                className="bg-white  text-center font-pB p-[15px] w-[100%] border-[2px] border-black"
-                to={"/page4"}
+                to="/page1"
+                className="relative bg-white text-center font-pB p-[15px] w-[100%] border-[2px] border-black overflow-hidden group"
               >
-                Stammbaum
-              </Link>
-              <Link
-                className="bg-white  text-center font-pB p-[15px] w-[100%] border-[2px] border-black"
-                to={"/page5"}
-              >
-                Die Restaurant
+                <span className="relative z-10 transition-colors duration-500 group-hover:text-white text-[20px]">
+                  Die Restaurant
+                </span>
+                <span className="absolute top-0 left-0 w-full h-1/3 bg-black scale-x-0 group-hover:scale-x-100 origin-left transition-transform duration-500" />
+                <span className="absolute top-1/3 left-0 w-full h-1/3 bg-red scale-x-0 group-hover:scale-x-100 origin-left transition-transform duration-700" />
+                <span className="absolute top-2/3 left-0 w-full h-1/3 bg-yellow scale-x-0 group-hover:scale-x-100 origin-left transition-transform duration-1000" />
               </Link>
             </div>
             <h1 className="text-[25px]  py-[20px] bg-dark p-[15px] font-pB w-[600px] text-white">
-              Unterstützt von : <span className="font-pL">NASSIM ELMHARMACHE</span> |{" "}
-              <span className="font-pL">HAMZA HAROUNE</span> | <span className="font-pL">HAMZA HAROUNE</span> | <span className="font-pL">HAMZA HAROUNE</span>
+              Unterstützt von :{" "}
+              <span className="font-pL">NASSIM ELMHARMACHE</span> |{" "}
+              <span className="font-pL">HAMZA HAROUNE</span> |{" "}
+              <span className="font-pL">Malak Rahma</span> |{" "}
+              <span className="font-pL">Hafssa Hamidi</span>
             </h1>
           </div>
           <div className="w-[600px] h-[600px]">
